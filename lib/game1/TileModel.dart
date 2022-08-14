5d4d3e78262184b1d01aee06944e3003437d6057
ADMIN <prathamjpm07@gmail.com>
2022-08-14 18:50:42 +0530

    i have added the game 1 memory game here

---
+++ lib/game1/TileModel.dart
@@ -0,0 +1,23 @@
+class TileModel{
+
+  String imageAssetPath="";
+  late bool isSelected;
+
+  TileModel();
+
+  void setImageAssetPath(String getImageAssetPath){
+    imageAssetPath = getImageAssetPath;
+  }
+
+  String getImageAssetPath(){
+    return imageAssetPath;
+  }
+
+  void setIsSelected(bool getIsSelected){
+    isSelected = getIsSelected;
+  }
+
+  bool getIsSelected(){
+    return isSelected;
+  }
+}
