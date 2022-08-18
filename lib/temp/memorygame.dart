0dc8ec914b37ee35f340450c1aa8c35aa375b40a
Arnav <arnav.rajesh07@gmail.com>
2022-08-18 18:35:19 +0530

    FireBase initialized

--- lib/temp/memorygame.dart
+++ lib/temp/memorygame.dart
@@ -1,9 +1,11 @@
 import 'dart:ffi';
 
 import 'package:flutter/material.dart';
-int points=0;
-int counter=1;
-bool start=true;
+
+int points = 0;
+int counter = 1;
+bool start = true;
+
 class Game2 extends StatefulWidget {
   const Game2({Key? key}) : super(key: key);
 
@@ -22,94 +24,87 @@ class _MemoryGameState extends State<Game2> {
       Button_ids.add(i);
     }
     Button_ids.shuffle();
-      for (int i = 0; i < n; i++) {
-        gameMatrix.add(GameTile(
-            id: Button_ids[i],
-            onPressed: () {
-              if (counter == Button_ids[i]) {
-                setState(() {
-                 points=points+20;
-                 counter++;
-                 gameMatrix;
-                });
-                print("pressed the right button");
-                print(counter);
-              }
-            },
-        ),
-        );
-      }
-
-    }
-
-    Widget game()
-    {
-      return Container(
-        padding: EdgeInsets.all(20),
-        child: Column(
-          children: [
-            GridView.count(
-              crossAxisCount: 5,
-              shrinkWrap: true,
-              children: gameMatrix,
-            )
-          ],
+    for (int i = 0; i < n; i++) {
+      gameMatrix.add(
+        GameTile(
+          id: Button_ids[i],
+          onPressed: () {
+            if (counter == Button_ids[i]) {
+              setState(() {
+                points = points + 20;
+                counter++;
+              });
+              print("pressed the right button");
+              print(counter);
+            }
+          },
         ),
       );
     }
+  }
+
+  Widget game() {
+    return Container(
+      padding: EdgeInsets.all(20),
+      child: Column(
+        children: [
+          GridView.count(
+            crossAxisCount: 5,
+            shrinkWrap: true,
+            children: gameMatrix,
+          )
+        ],
+      ),
+    );
+  }
 
   void initState() {
     getListofNums();
     counter = 1;
-    points=0;
+    points = 0;
   }
 
   bool start = false;
 
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          "Test Your Speed",
-          style: TextStyle(fontSize: 25, color: Colors.white),
+        appBar: AppBar(
+          title: Text(
+            "Test Your Speed",
+            style: TextStyle(fontSize: 25, color: Colors.white),
+          ),
         ),
-      ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          Text(
-            "$points/600",
-            style: TextStyle(
-                fontSize: 20, fontWeight: FontWeight.w500),
-          ),
-          Text(
-            "Points",
-            textAlign: TextAlign.start,
-            style: TextStyle(
-                fontSize: 14, fontWeight: FontWeight.w300),
-          ),
-          Text(
-            "Hit the Rabbit",
-            style: TextStyle(fontSize: 25, color: Colors.blue),
-          ),
-          Center(
-            child: game()
-          ),]
-    )
-    );
+        body: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text(
+                "$points/600",
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.w500),
+              ),
+              Text(
+                "Points",
+                textAlign: TextAlign.start,
+                style: TextStyle(fontSize: 14, fontWeight: FontWeight.w300),
+              ),
+              Text(
+                "Hit the Rabbit",
+                style: TextStyle(fontSize: 25, color: Colors.blue),
+              ),
+              Center(child: game()),
+            ]));
   }
 }
 
 class GameTile extends StatefulWidget {
   int id;
   late Function() onPressed;
-  GameTile({required this.id,required this.onPressed});
+  GameTile({required this.id, required this.onPressed});
   @override
   State<GameTile> createState() => _GameTileState();
 }
 
 class _GameTileState extends State<GameTile> {
-bool selected=true;
+  bool selected = true;
   @override
   void initState() {
     Future.delayed(Duration(seconds: 20), () {
@@ -118,23 +113,24 @@ bool selected=true;
       });
     });
   }
+
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
-      onTap: ()
-        {
+        onTap: () {
           setState(() {
             widget.onPressed();
-            selected=true;
+            selected = true;
           });
         },
-      child:counter==widget.id?Container(
-          margin: const EdgeInsets.all(5),
-          child: Image.asset("assets/rabbit.png"),
-      ):Container(
-        margin: const EdgeInsets.all(5),
-        child: Image.asset("assets/panda.png"),
-      )
-    );
+        child: counter == widget.id
+            ? Container(
+                margin: const EdgeInsets.all(5),
+                child: Image.asset("assets/rabbit.png"),
+              )
+            : Container(
+                margin: const EdgeInsets.all(5),
+                child: Image.asset("assets/panda.png"),
+              ));
   }
 }
