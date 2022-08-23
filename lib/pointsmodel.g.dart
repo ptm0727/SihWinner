431434619979b8c04fd5abed20ed12a8f9e071af
ADMIN <prathamjpm07@gmail.com>
2022-08-23 11:58:19 +0530

    ..

--- lib/pointsmodel.g.dart
+++ lib/pointsmodel.g.dart
@@ -23,13 +23,19 @@ class pointsmodelAdapter extends TypeAdapter<pointsmodel> {
       p4: fields[3] as double,
       p5: fields[4] as double,
       p6: fields[5] as double,
+      d1: fields[6] as double,
+      d2: fields[7] as double,
+      d3: fields[8] as double,
+      d4: fields[9] as double,
+      d5: fields[10] as double,
+      d6: fields[11] as double,
     );
   }
 
   @override
   void write(BinaryWriter writer, pointsmodel obj) {
     writer
-      ..writeByte(6)
+      ..writeByte(12)
       ..writeByte(0)
       ..write(obj.p1)
       ..writeByte(1)
@@ -41,7 +47,19 @@ class pointsmodelAdapter extends TypeAdapter<pointsmodel> {
       ..writeByte(4)
       ..write(obj.p5)
       ..writeByte(5)
-      ..write(obj.p6);
+      ..write(obj.p6)
+      ..writeByte(6)
+      ..write(obj.d1)
+      ..writeByte(7)
+      ..write(obj.d2)
+      ..writeByte(8)
+      ..write(obj.d3)
+      ..writeByte(9)
+      ..write(obj.d4)
+      ..writeByte(10)
+      ..write(obj.d5)
+      ..writeByte(11)
+      ..write(obj.d6);
   }
 
   @override
